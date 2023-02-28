6ae9f7736080f830714d964b16d78615b73c656b
Thomas Bacheley <79910466+ThomasBacheley@users.noreply.github.com>
2023-02-28 15:10:51 +0100

    get books

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import './App.css'
+import "./App.css";
+import data from './data.json';
 
 import ListBook from "./components/ListBook";
 
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-  const apikey = 'AIzaSyCJ4CtX0Bz0ONtdgzyUCohSsx5RKV9IZL0';
+  const apikey = "AIzaSyCJ4CtX0Bz0ONtdgzyUCohSsx5RKV9IZL0";
 
-  var books = [];
-
-  axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers&key='+apikey).then(response=>{
-    response.data.items.forEach(book => {
-      var book = {
-        id:book.id,
-        name:book.volumeInfo.title,
-        authorname:'Inconnu'
-      }
-      books = [...books,book];
+  var books = [{
+    id:'U0nxVnZlsosC',
+    name:'Business development',
+    authors:'Jérôme Gayet'
+  }];
+  
+  /* await axios
+    .get("https://www.googleapis.com/books/v1/volumes?q=development&key=" + apikey)
+    .then((response) => {
+      console.log(response.data)
+      response.data.items.forEach((book) => {
+        var book = {
+          id: book.id,
+          name: book.volumeInfo.title,
+          authorname: "Inconnu",
+        };
+        books = [...books, book];
+      });
     });
-  });
+ */
+    console.log(books);
 
   return (
     <div className="App">
       <ListBook books={books}></ListBook>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
