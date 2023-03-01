488fe57ad89bd745e0753e14226ca3e64d1c1073
Thomas Bacheley <79910466+ThomasBacheley@users.noreply.github.com>
2023-03-01 09:48:31 +0100

    update and add label to input

--- src/components/FormLog.jsx
+++ src/components/FormLog.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const FormLog = () => (
-  <div>
-    <h1>Anywhere in your app!</h1>
+  <div className="border border-dark rounded w-50 center">
+    <h1>Any place in your app!</h1>
     <Formik
       initialValues={{ email: "", password: "" }}
       validate={(values) => {
@@ -19,42 +19,29 @@ const FormLog = () => (
       }}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
+          let JSONValue = JSON.stringify(values, null, 2); //on stock le contenue des inputs au format json
+          alert(JSONValue);
           setSubmitting(false);
         }, 400);
       }}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <label htmlFor="email">Email Address :</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </div>
+          <div>
+            <label htmlFor="email">Password :</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
+
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
-        </form>
+        </Form>
       )}
     </Formik>
   </div>
