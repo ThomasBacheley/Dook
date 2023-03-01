8355ea9d6b52a55462c0b2206c75952b7b10950b
Thomas Bacheley <79910466+ThomasBacheley@users.noreply.github.com>
2023-03-01 15:05:27 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,44 @@ import MyHeaders from "./components/MyHeaders";
 
 import axios from "axios";
 import redux from "./redux";
+import { useEffect, useState } from "react";
+
+async function getBooksAPI(url) {
+  axios.get(url).then((response) => {
+    console.log(response.data.items.length);
+    response.data.items.forEach((book) => {
+      var book = {
+        id: book.id,
+        title: book.volumeInfo.title,
+        author: book.volumeInfo.authors
+          ? book.volumeInfo.authors.join(", ")
+          : "Inconnu",
+      };
+      redux.dispatch({ type: "book/addBook", payload: book });
+    });
+  });
+}
 
 function App() {
-  var go = false;
-  axios
-    .get(
-      "https://www.googleapis.com/books/v1/volumes?q=$%7Bexample%7D&maxResults=20"
-    )
-    .then((response) => {
-      response.data.items.forEach((book) => {
-        var book = {
-          id: book.id,
-          title: book.volumeInfo.title,
-          author: "Inconnu",
-        };
-        redux.dispatch({type:'book/addBook',payload:book});
-      });
-    });
+  const [research, setResearch] = useState("book");
+
+  useEffect(() => {
+    console.log(research)
+    getBooksAPI(
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+        research +
+        "&maxResults=30"
+    );
+  });
+
+  const editResearch = (research) => {
+    setResearch(research);
+  };
 
   return (
     <div className="bg-perso1 pt-3 h-100">
       <div className="container">
-        <MyHeaders/>
+        <MyHeaders editResearch={editResearch} />
         <div className="d-flex justify-content-between">
           <div>
             <h1>Book List</h1>
